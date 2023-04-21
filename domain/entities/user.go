4cf1a18b341ec4c07aec752ebd961bf7d021d5aa
GABRIEL.BELLINI <gabriel.bellini@lince.com.br>
2023-04-21 10:20:26 -0300

    change user entity

--- domain/entities/user.go
+++ domain/entities/user.go
@@ -3,10 +3,10 @@ package entities
 import "base/util"
 
 type User struct {
-	Id         int           `json:"id"`
+	Id         int64         `json:"id"`
 	Name       string        `json:"name"`
 	Credential Credential    `json:"credential"`
 	CreatedAt  util.DateTime `json:"createdAt"`
 	ModifiedAt util.DateTime `json:"modifiedAt"`
-	StatusCode string        `json:"statusCode"`
+	StatusCode int64         `json:"statusCode"`
 }
