ed2e61b9547ccb51862765980ae4e42f483f2918
Felipe A. de Souza <FELIPE.DESOUZA@lince.com.br>
2023-04-21 09:28:28 -0300

    ddls to create tables: resolution and user

--- scripts/ddls/01_ddl_resolution.sql
+++ scripts/ddls/01_ddl_resolution.sql
@@ -1,9 +1,8 @@
-CREATE TABLE IF NOT EXISTS device
+CREATE TABLE IF NOT EXISTS resolution
 (
     id                      INTEGER       NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name                    VARCHAR(60)   NOT NULL DEFAULT '',
-    id_resolution           INTEGER       NOT NULL,
-    id_orientation          INTEGER       NOT NULL DEFAULT 0,
+    width                   INTEGER       NOT NULL,
+    height                  INTEGER       NOT NULL,
     status_code             TINYINT       NOT NULL DEFAULT 0,
     created_at              DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP,
     modified_at             DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
--- scripts/ddls/02_ddl_user.sql
+++ scripts/ddls/02_ddl_user.sql
@@ -1,9 +1,10 @@
-CREATE TABLE IF NOT EXISTS device
+CREATE TABLE IF NOT EXISTS user
 (
     id                      INTEGER       NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name                    VARCHAR(60)   NOT NULL DEFAULT '',
-    id_resolution           INTEGER       NOT NULL,
-    id_orientation          INTEGER       NOT NULL DEFAULT 0,
+    email                   VARCHAR(60)   NOT NULL,
+    password                TEXT          NOT NULL,
+    role                    TEXT          NOT NULL,
     status_code             TINYINT       NOT NULL DEFAULT 0,
     created_at              DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP,
     modified_at             DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
