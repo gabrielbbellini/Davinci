b138eaa25cd5fd9a4b5b3ef306f4a3c4f83724d9
GABRIEL.BELLINI <gabriel.bellini@lince.com.br>
2023-04-22 17:27:15 -0300

    ddl

--- scripts/ddls/03_ddl_presentation.sql
+++ scripts/ddls/03_ddl_presentation.sql
@@ -16,18 +16,18 @@ ALTER TABLE presentation
 CREATE TABLE IF NOT EXISTS page
 (
     id              INTEGER  NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    id_presentation INTEGER  NOT NULL,
-    timing          INTEGER  NOT NULL,
-    metadata        JSON     NOT NULL,
+    id_presentation INTEGER  NOT NULL REFERENCES presentation (id),
+    duration        INTEGER  NOT NULL,
+    component       JSON     NOT NULL,
     status_code     TINYINT  NOT NULL DEFAULT 0,
     created_at      DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     modified_at     DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS presentation_per_device
+CREATE TABLE IF NOT EXISTS device_presentation
 (
-    id_device       INTEGER  NOT NULL,
-    id_presentation INTEGER  NOT NULL,
+    id_device       INTEGER  NOT NULL REFERENCES device (id),
+    id_presentation INTEGER  NOT NULL REFERENCES presentation (id),
     is_playing      INTEGER  NOT NULL DEFAULT 0,
     status_code     TINYINT  NOT NULL DEFAULT 0,
     created_at      DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
