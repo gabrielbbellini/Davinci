a7da758e1b417c4ce116da085c19edd21f475e2f
Gabriel Bellini <gabrielbritobellini@gmail.com>
2023-04-23 16:48:11 -0300

    remove redundant method on cors

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func main() {
 		Handler: handlers.CORS(
 			handlers.AllowedOrigins([]string{"*"}),
 			handlers.AllowedHeaders([]string{"Authorization", "Content-Type", "Accept", "Authorization"}),
-			handlers.AllowedMethods([]string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete, "UPDATE"}),
+			handlers.AllowedMethods([]string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete}),
 			handlers.AllowCredentials(),
 		)(router),
 		Addr:         ServerUrl,
